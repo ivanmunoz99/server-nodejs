afdc4afc50d053fa97667960753236bb0a4ec4dc
ivanmunoz99 <ivandariomunos99@gmail.com>
2023-01-25 09:24:28 -0500

    port

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hola mundo")
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get("/",(req,res) => {
+  res.send("hola nn")
+})
+
+app.listen(port, () => {
+  console.log("mi puerto es " + port)
+})
